1d8a4483da9b87bc45fb1ac7b770c84ab54f193b
Johann Frei <johann_frei@yahoo.de>
2018-11-27 16:07:34 +0100

    fixed minor path issues

--- DataSets/OASIS3.py
+++ DataSets/OASIS3.py
@@ -6,13 +6,13 @@ from DataSets.DataLoader import DataSet
 class OASIS3(DataSet):
     def __init__(self):
         path = "/data/johann/oasis3/OAS3*MR_*/anat*/NIFTI/*.nii.gz"
-        path = "/mnt/hdd1/oasis1/out/OAS3*MR_*/anat*/NIFTI/*.nii.gz"
+        path = "/mnt/hdd1/oasis3/OAS3*MR_*/anat*/NIFTI/*.nii.gz"
         subjects = list(glob(path))
 
         def subjectProperties(subj):
             props = {}
             pattern = re.compile(r'OAS3[^_]*_MR_d(?P<id>[0-9]+)/anat(?P<anat>[0-9]+)')
-            oasis_id,anat_id = pattern.findall(subj_root)[0]
+            oasis_id,anat_id = pattern.findall(subj)[0]
 
             props['id'] = "{}-{}".format(str(oasis_id),str(anat_id))
             props['img'] = subj
